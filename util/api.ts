496ef265d22f6a24e82ce3b24fc37f033acb8f7d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-09 20:43:13 -0500

    Add profile api route

--- util/api.ts
+++ util/api.ts
@@ -3,5 +3,6 @@ export const base = 'http://localhost:3001/'
 export const api = {
   home: base,
   google: base + 'auth/google',
-  google_redirect: base + 'auth/google/redirect'
+  google_redirect: base + 'auth/google/redirect',
+  profile: base + 'auth/profile'
 };
