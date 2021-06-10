91d2ec31f9abd80bc2a6429cee714480bc403909
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-09 21:30:27 -0500

    Refactor authentication code to separate method

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,10 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import axios from 'axios';
-import { api } from '../util/api';
-import { authStore, login, logout } from '../store/auth-store';
 import { useEffect } from 'react';
+import authenticate from '../util/authenticate';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const accessToken = localStorage.getItem('access_token')
-    if (!accessToken || accessToken.trim() === '')
-      return
-
-    axios.get(api.profile, {
-      headers: { 'Authorization': `Bearer ${accessToken}` }
-    })
-      .then(({ data }) => {
-        authStore.dispatch(login({
-          accessToken: accessToken,
-          user: data.user,
-          gToken: data.gToken
-        }))
-      })
-      .catch(_ => {
-        authStore.dispatch(logout())
-        console.log('You were logged out!')
-      })
-  })
+  useEffect(() => authenticate())
 
   return (
     <ChakraProvider>
---
+++ util/authenticate.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { api } from '../util/api';
+import { authStore, login, logout } from '../store/auth-store';
+
+export default function authenticate() {
+  const accessToken = localStorage.getItem('access_token')
+  if (!accessToken || accessToken.trim() === '')
+    return
+
+  axios.get(api.profile, {
+    headers: { 'Authorization': `Bearer ${accessToken}` }
+  })
+    .then(({ data }) => {
+      authStore.dispatch(login({
+        accessToken: accessToken,
+        user: data.user,
+        gToken: data.gToken
+      }))
+    })
+    .catch(_ => {
+      authStore.dispatch(logout())
+      console.log('You were logged out!')
+    })
+}
