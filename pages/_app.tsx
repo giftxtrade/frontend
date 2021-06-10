f753a0e24c61747efa6393ca94c6920b2de4721a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-09 20:43:57 -0500

    Upadate state after verification of access_token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import axios from 'axios';
+import { api } from '../util/api';
+import { authStore, login, logout } from '../store/auth-store';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token')
+    if (!accessToken || accessToken.trim() === '')
+      return
+
+    axios.get(api.profile, {
+      headers: { 'Authorization': `Bearer ${accessToken}` }
+    })
+      .then(({ data }) => {
+        authStore.dispatch(login({
+          accessToken: accessToken,
+          user: data.user,
+          gToken: data.gToken
+        }))
+      })
+      .catch(_ => {
+        authStore.dispatch(logout())
+        console.log('You were logged out!')
+      })
+  })
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
