3deaf215151c25d1fd202c38e3c7c19226e07d61
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-10 22:03:00 -0500

    Add user profile + props

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,35 @@
-import { Container, Image, Flex } from "@chakra-ui/react";
+import { Container, Image, Flex, Heading, Text, Link, Button, Box } from "@chakra-ui/react";
+import { User } from '../store/jwt-payload';
+import { authStore, logout } from '../store/auth-store';
+import router from "next/router";
 
-export default function Navbar({ auth }: any) {
+export default function Navbar({ loggedIn, user, accessToken, gToken }: {
+  loggedIn: boolean,
+  user: User,
+  accessToken: string,
+  gToken: string
+}) {
   return (
     <Container maxW='4xl' p='5' mb='10'>
       <Flex direction='row' justifyContent='space-between' alignItems='center'>
         <Image w='48' src='/giftxtrade_logo_color.svg' />
+
+        <Flex
+          direction="row"
+          alignItems="center"
+          justifyContent='start'
+          cursor='pointer'
+          onClick={() => {
+            authStore.dispatch(logout())
+            // router.push('/')
+          }}
+        >
+          <Image src={user.imageUrl} w='40px' mr='3' rounded='md' />
+          <Box>
+            <Heading size='sm'>{user.name}</Heading>
+            <Text fontSize='xs'>{user.email}</Text>
+          </Box>
+        </Flex>
       </Flex>
     </Container>
   )
