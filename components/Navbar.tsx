aeb5f8928f6a5474a7d0a37ecfdcc742364478d5
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-14 17:20:58 -0500

    Remove cookie on logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Container, Image, Flex, Heading, Text, Link, Button, Box } from "@chakr
 import { User } from '../store/jwt-payload';
 import { authStore, logout } from '../store/auth-store';
 import router from "next/router";
+import { useCookies } from "react-cookie";
 
 export default function Navbar({ loggedIn, user, accessToken, gToken }: {
   loggedIn: boolean,
@@ -9,6 +10,8 @@ export default function Navbar({ loggedIn, user, accessToken, gToken }: {
   accessToken: string,
   gToken: string
 }) {
+  const [cookie, setCookie, removeCookie] = useCookies(['access_token'])
+
   return (
     <Container maxW='4xl' p='5' mb='10'>
       <Flex direction='row' justifyContent='space-between' alignItems='center'>
@@ -20,6 +23,7 @@ export default function Navbar({ loggedIn, user, accessToken, gToken }: {
           justifyContent='start'
           cursor='pointer'
           onClick={() => {
+            removeCookie('access_token')
             authStore.dispatch(logout())
             router.push('/')
           }}
