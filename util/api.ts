712b6db24bd33b6ada0b9ab1a03017dab51d1708
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-14 20:26:10 -0500

    Add products endpoint

--- util/api.ts
+++ util/api.ts
@@ -4,5 +4,6 @@ export const api = {
   home: base,
   google: base + 'auth/google',
   google_redirect: base + 'auth/google/redirect',
-  profile: base + 'auth/profile'
+  profile: base + 'auth/profile',
+  products: base + 'products', // GET -> Params: limit, page, min_price, max_price, search
 };
