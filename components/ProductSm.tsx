3e9b37c5085ddf107a81c843d61ec2a8b3a993f2
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-14 23:39:58 -0500

    Add link

--- components/ProductSm.tsx
+++ components/ProductSm.tsx
@@ -3,19 +3,19 @@ import { Box, Image, Heading, Text, Button, Link } from '@chakra-ui/react';
 import { generateAmazonAffiliateLink } from "../util/links";
 
 export default function ProductSm({ product }: { product: IProduct }) {
+  const link = generateAmazonAffiliateLink(product.productKey)
   return (
     <Box mb='5'>
-      <Image
-        src={product.imageUrl}
-        loading='lazy'
-        rounded='md'
-        mb='3'
-      />
-      <Heading size='sm' noOfLines={3} mb='2'>{product.title}</Heading>
-      <Text>${product.price}</Text>
-      <Link href={generateAmazonAffiliateLink(product.productKey)} target='blank' isExternal={true}>
-        <Button mt='4' size='sm'>Original link</Button>
+      <Link href={link} target='blank' isExternal={true}>
+        <Image
+          src={product.imageUrl}
+          loading='lazy'
+          rounded='md'
+          mb='3'
+        />
+        <Heading size='sm' noOfLines={3} mb='2'>{product.title}</Heading>
       </Link>
+      <Text textDecoration='none'>${product.price}</Text>
     </Box>
   )
 }
