fa82b4014492ffe5e312fdb709387199e379e718
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-20 12:18:35 -0500

    Add new participant and event types

--- types/Event.ts
+++ types/Event.ts
@@ -1,4 +1,4 @@
-import { ICreateParticipant } from './Participant';
+import { ICreateParticipant, IParticipant } from './Participant';
 
 export interface IEvent {
   id: number
@@ -7,16 +7,17 @@ export interface IEvent {
   budget: number
   invitationMessage: string
   createdAt: string
-  drawAt: string;
-  closeAt: string;
+  drawAt: string
+  closeAt: string
+  participants: IParticipant[]
 }
 
 export interface ICreateEvent {
-  name: string;
-  description: string;
-  budget: number;
-  invitationMessage: string;
-  drawAt: Date;
-  closeAt: Date;
+  name: string
+  description: string
+  budget: number
+  invitationMessage: string
+  drawAt: Date
+  closeAt: Date
   participants: ICreateParticipant[]
 }
--- types/Participant.ts
+++ types/Participant.ts
@@ -1,8 +1,18 @@
+export interface IParticipant {
+  id: number
+  name: string
+  email: string
+  address: string
+  organizer: boolean
+  participates: boolean
+  accepted: boolean
+}
+
 export interface ICreateParticipant {
-  name: string;
-  email: string;
-  address: string;
-  organizer: boolean;
-  participates: boolean;
-  accepted: boolean;
+  name: string
+  email: string
+  address: string
+  organizer: boolean
+  participates: boolean
+  accepted: boolean
 }
