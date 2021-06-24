705778cfeef3e0d60ea0b49234aab01698b6c59d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-24 15:41:44 -0500

    Add API endpoint for wishes

--- util/api.ts
+++ util/api.ts
@@ -12,5 +12,6 @@ export const api = {
   decline_invite: base + 'events/invites/decline', // Auth required, [GET], param required eventID
   get_link: base + 'events/get-link', // Auth required, [POST], param required eventId
   verify_invite_code: base + 'events/verify-invite-code', // [GET], param required invite code
-  invite_code: base + 'events/invite-code' // Auth required, [GET], param required invite code
+  invite_code: base + 'events/invite-code', // Auth required, [GET], param required invite code
+  wishes: base + 'wishes' // Auth required, [POST], [GET] :eventId, [DELETE] wishId
 };
