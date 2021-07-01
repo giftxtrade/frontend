f0db4e3ec913c88b6628b5b59eb02fe37a3d23ff
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-30 21:12:09 -0500

    Add participant page

---
+++ pages/events/[id]/[email].tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Box,
+  Stack,
+  Container,
+} from '@chakra-ui/react';
+import Head from 'next/head';
+import Navbar from '../../../components/Navbar';
+import { DocumentContext } from "next/document";
+import Search from "../../../components/Search";
+import eventFetch from "../../../util/ss-event-fetch";
+import { IEventProps } from "../[id]";
+import { useMediaQuery } from '@chakra-ui/react';
+import { IWish } from '../../../types/Wish';
+
+export default function ParticipantPage(props: IEventProps) {
+  const [loggedIn, setLoggedIn] = useState(props.loggedIn)
+  const [accessToken, setAccessToken] = useState(props.accessToken)
+  const [gToken, setGToken] = useState(props.gToken)
+  const [user, setUser] = useState(props.user)
+  const [event, setEvent] = useState(props.event)
+  const [meParticipant, setMeParticipant] = useState(props.meParticipant)
+  const [loadingWishes, setLoadingWishes] = useState(true)
+  const [wishes, setWishes] = useState(Array<IWish>())
+  const [wishProductIds, setWishProductIds] = useState(new Set<number>())
+
+  // Media queries
+  const [isMediumScreen] = useMediaQuery('(max-width: 900px)')
+
+  return (
+    <>
+      <Head>
+        <title>Participant | {event.name} - GiftTrade</title>
+      </Head>
+
+      <Navbar
+        loggedIn={loggedIn}
+        accessToken={accessToken}
+        user={user}
+        gToken={gToken}
+      />
+
+      <Container maxW='4xl' mb='20'>
+        <Flex direction='row'>
+          <Container
+            flex='2'
+            pl='0'
+          >
+            <Heading>Participant</Heading>
+          </Container>
+
+          {isMediumScreen ? (
+            <></>
+          ) : (
+            <Container
+              flex='1'
+              pl='2'
+              pr='0'
+            >
+            </Container>
+          )}
+        </Flex>
+      </Container>
+    </>
+  )
+}
+
+export const getServerSideProps = async (ctx: DocumentContext) => eventFetch(ctx);
