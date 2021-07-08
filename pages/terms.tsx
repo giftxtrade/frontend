fe6c0c23f33749e9b67466b8e5d8d6819a5fb97c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-07-08 16:30:14 -0500

    Add terms page with redirect

---
+++ pages/terms.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+export default function Terms() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/privacy')
+  }, [])
+
+  return (
+    <Head>Redirecting to Privacy Page...</Head>
+  )
+}
