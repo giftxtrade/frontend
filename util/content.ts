162a8be6c9a4ecb1d42ed8a8e579946f37d04007
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-07-18 22:02:24 -0500

    Update Amazon Associates store id

--- util/content.ts
+++ util/content.ts
@@ -1,5 +1,5 @@
 export const content = {
-    ASSOCIATE_TAG: "none0468-20",
+    ASSOCIATE_TAG: "giftxtrade0a-20",
     GTAG: "G-2WM8TF71MK",
     BASE_TITLE: "Gift Exchange, Made Simple - GiftTrade",
     DESCRIPTION: "Make your Gift Exhange and Secret Santa simple and secure with GiftTrade"
