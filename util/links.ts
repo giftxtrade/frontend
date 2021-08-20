31a559cb737b7f4399198bf6e493df7c6ac74de3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-08-20 00:01:03 -0500

    Slugify

--- util/links.ts
+++ util/links.ts
@@ -1,5 +1,10 @@
 import { content } from './content'
+import slugify from 'slugify';
 
 export const generateAmazonAffiliateLink = (productKey: string): string => {
   return `https://www.amazon.com/dp/${productKey}?tag=${content.ASSOCIATE_TAG}&linkCode=ll1&ie=UTF8&psc=1`
+}
+
+export function eventNameSlug(eventName: string): string {
+  return slugify(eventName)
 }
