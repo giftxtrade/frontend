c5d480a1a569a34b7c408b8fe025df4f2a641e08
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-08-26 11:51:19 -0500

    Fix affiliate link url

--- util/links.ts
+++ util/links.ts
@@ -2,7 +2,7 @@ import { content } from './content'
 import slugify from 'slugify';
 
 export const generateAmazonAffiliateLink = (productKey: string): string => {
-  return `https://www.amazon.com/dp/${productKey}?tag=${content.ASSOCIATE_TAG}&linkCode=ll1&ie=UTF8&psc=1`
+  return `https://www.amazon.com/dp/${productKey}?tag=${content.ASSOCIATE_TAG}`
 }
 
 export function eventNameSlug(eventName: string): string {
