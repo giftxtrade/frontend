3c4787304e0d65a294d7c84c55c7a833d37919b0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-06 15:05:15 -0500

    Change Google profile picture size

--- util/content.ts
+++ util/content.ts
@@ -1,14 +1,20 @@
 export const content = {
-    ASSOCIATE_TAG: "giftxtrade0a-20",
-    GTAG: "G-2WM8TF71MK",
-    BASE_TITLE: "Gift Exchange, Made Simple - GiftTrade",
-    DESCRIPTION: "Make your Gift Exhange and Secret Santa simple and secure with GiftTrade"
+  ASSOCIATE_TAG: "giftxtrade0a-20",
+  GTAG: "G-2WM8TF71MK",
+  BASE_TITLE: "Gift Exchange, Made Simple - GiftTrade",
+  DESCRIPTION: "Make your Gift Exhange and Secret Santa simple and secure with GiftTrade"
 }
 
 export const toStringOrUndefined = (val: string[] | string | undefined) => {
-    return typeof (val) === 'object' ? val[0] : val;
+  return typeof (val) === 'object' ? val[0] : val;
 }
 
 export const toStringOrNull = (val: string[] | string | undefined) => {
-    return typeof (val) === 'object' ? val[0] : val ? val : null;
+  return typeof (val) === 'object' ? val[0] : val ? val : null;
+}
+
+export const changeProfileSize = (imageUrl: string, size: number): string => {
+  const imageUrlIdentifier = imageUrl.split('=');
+  const main = imageUrlIdentifier[0];
+  return `${main}=s${size}`;
 }
