bce4cba34b2009969f26668114e389065b5808ab
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-21 17:43:01 -0500

    Added new types

--- types/Draw.ts
+++ types/Draw.ts
@@ -1,8 +1,15 @@
-import { IParticipant } from './Participant';
+import { IParticipant, IParticipantUser } from './Participant';
 
 export interface IDraw {
   id: number
   createdAt: string
   drawer: IParticipant
   drawee: IParticipant
+}
+
+export interface IDrawParticipant {
+  id: number
+  createdAt: string
+  drawer: IParticipantUser
+  drawee: IParticipantUser
 }
--- types/Event.ts
+++ types/Event.ts
@@ -1,6 +1,7 @@
+import { ILink } from './Link';
 import { ICreateParticipant, IParticipant, IParticipantUser } from './Participant';
 
-interface IEventBase {
+export interface IEventBase {
   id: number
   name: string
   description: string
@@ -17,6 +18,16 @@ export interface IEvent extends IEventBase {
 
 export interface IEventUser extends IEventBase {
   participants: IParticipantUser[]
+}
+
+export interface IEventFull extends IEventUser {
+  links: ILink[]
+}
+
+export interface IEventDetails {
+  id: number
+  name: string
+  description: string
 }
 
 export interface ICreateEvent {
