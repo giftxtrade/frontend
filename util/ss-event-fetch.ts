8feca3102b1bf7704e5d9b627dd054fcc08ef52b
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-21 17:50:26 -0500

    Update server-side event fetch

--- util/ss-event-fetch.ts
+++ util/ss-event-fetch.ts
@@ -1,6 +1,6 @@
 import { DocumentContext } from 'next/document';
 import { serverSideAuth } from './server-side-auth';
-import { IEvent } from '../types/Event';
+import { IEvent, IEventDetails, IEventFull } from '../types/Event';
 import { IParticipantUser, IParticipant } from '../types/Participant';
 import { ILink } from '../types/Link';
 import axios from 'axios';
@@ -10,20 +10,17 @@ import { User } from '../store/jwt-payload';
 import { eventNameSlug } from './links';
 
 export interface IServiceSideEventProps {
-  accessToken: string | undefined,
-  user: User | undefined,
-  gToken: string | undefined,
-  loggedIn: boolean | undefined,
-  event: IEvent,
-  participants: IParticipantUser[],
-  link: ILink | undefined,
-  meParticipant: IParticipant | undefined
+  accessToken: string | undefined
+  user: User | undefined
+  gToken: string | undefined
+  loggedIn: boolean | undefined
+  eventDetails: IEventDetails
 }
 
 export interface IServerSideEventFetch {
-  props: IServiceSideEventProps | undefined,
-  notFound: boolean,
-  redirect: { destination: string } | undefined,
+  props: IServiceSideEventProps | undefined
+  notFound: boolean
+  redirect: { destination: string } | undefined
 }
 
 export default async function eventFetch(ctx: DocumentContext): Promise<IServerSideEventFetch> {
@@ -38,32 +35,28 @@ export default async function eventFetch(ctx: DocumentContext): Promise<IServerS
 
   const { props } = await serverSideAuth(ctx)
 
-  let event: IEvent | undefined;
-  let participants: IParticipantUser[] = [];
-  let link: ILink | undefined;
+  let eventDetails: IEventDetails | undefined
   if (props.loggedIn) {
-    await axios.get(`${api.events}/${idRaw}`, {
+    await axios.get(`${api.events}/${idRaw}?verify=true`, {
       headers: { "Authorization": "Bearer " + props.accessToken }
     })
-      .then(({ data }: { data: { event: IEvent, participants: IParticipantUser[], link: ILink } }) => {
-        event = data.event
-        participants = data.participants
-        link = data.link
+      .then(({ data }: { data: IEventDetails }) => {
+        eventDetails = data
       })
       .catch(_ => { })
   }
 
-  if (!event) {
+  if (!eventDetails) {
     return getProps(undefined, true, undefined)
   }
 
-  const eventSlug = eventNameSlug(event.name);
+  const eventSlug = eventNameSlug(eventDetails.name);
 
   if (eventName) {
     if (eventName !== eventSlug)
-      return getProps(undefined, false, `/events/${event.id}/${eventNameSlug(event.name)}`)
+      return getProps(undefined, false, `/events/${eventDetails.id}/${eventNameSlug(eventDetails.name)}`)
   } else if (eventSlug !== '') {
-    return getProps(undefined, false, `/events/${event.id}/${eventNameSlug(event.name)}`)
+    return getProps(undefined, false, `/events/${eventDetails.id}/${eventNameSlug(eventDetails.name)}`)
   }
 
   return getProps({
@@ -71,10 +64,7 @@ export default async function eventFetch(ctx: DocumentContext): Promise<IServerS
     user: props.user,
     gToken: props.gToken,
     loggedIn: props.loggedIn,
-    event: event,
-    participants: participants,
-    link: link,
-    meParticipant: getMeParticipant(event, props.user)
+    eventDetails: eventDetails
   }, false, undefined)
 }
 
@@ -86,34 +76,4 @@ function getProps(props: IServiceSideEventProps | undefined, notFound: boolean, 
       destination
     } : undefined
   }
-}
-
-export async function fetchMyDraw(eventId: number, accessToken: string): Promise<IParticipant | null> {
-  let myDraw: IParticipant | null = null
-  await axios.get(`${api.draws}/me/${eventId}`, {
-    headers: { "Authorization": "Bearer " + accessToken }
-  })
-    .then(({ data }: { data: IDraw }) => {
-      myDraw = data.drawee
-    })
-    .catch(err => {
-      myDraw = null
-    })
-  return myDraw
-}
-
-/**
- * Gets the current user's participant status
- * 
- * @param event Current Event
- * @param user JWT User object
- * @returns IParticipant
- */
-function getMeParticipant(event: IEvent, user: User | undefined): IParticipant | undefined {
-  for (const p of event.participants) {
-    if (p.email === user?.email) {
-      return p
-    }
-  }
-  return undefined
 }
