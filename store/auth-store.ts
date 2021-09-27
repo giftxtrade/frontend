354eb8795b0d1096f4585f2344729657a63be7da
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-27 12:57:59 -0500

    Fix types for redux state

--- store/auth-store.ts
+++ store/auth-store.ts
@@ -1,39 +1,33 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
-import JwtPayload from './jwt-payload';
+import JwtPayload, { AuthState, User } from "./jwt-payload";
 
 export const authSlice = createSlice({
-  name: 'auth',
-  initialState: {
-    accessToken: '',
-    user: { id: 0, name: '', email: '', imageUrl: '' },
-    gToken: '',
-    loggedIn: false
-  },
+  name: "auth",
+  initialState: {} as AuthState,
   reducers: {
     login: (state, { payload }: { payload: JwtPayload }) => {
-      if (!payload.accessToken || payload.accessToken.trim() === '')
-        return;
+      if (!payload.accessToken || payload.accessToken.trim() === "") return;
 
-      state.loggedIn = true
-      state.accessToken = payload.accessToken
-      state.gToken = payload.gToken
+      state.loggedIn = true;
+      state.accessToken = payload.accessToken;
+      state.gToken = payload.gToken;
       state.user = {
         id: payload.user.id,
         name: payload.user.name,
         email: payload.user.email,
         imageUrl: payload.user.imageUrl,
-      }
+      };
     },
-    logout: state => {
-      localStorage.removeItem('access_token')
+    logout: (state) => {
+      localStorage.removeItem("access_token");
 
-      state.loggedIn = false
-      state.user = { id: 0, name: '', email: '', imageUrl: '' }
-      state.gToken = ''
-      state.accessToken = ''
-    }
-  }
-})
+      state.loggedIn = false;
+      state.user = { id: 0, name: "", email: "", imageUrl: "" };
+      state.gToken = "";
+      state.accessToken = "";
+    },
+  },
+});
 
 export const { login, logout } = authSlice.actions
 
--- store/jwt-payload.ts
+++ store/jwt-payload.ts
@@ -5,6 +5,13 @@ export interface User {
   imageUrl: string;
 }
 
+export interface AuthState {
+  accessToken: string;
+  user: User;
+  gToken: string;
+  loggedIn: boolean;
+}
+
 export default interface JwtPayload {
   user: User;
   gToken: string;
