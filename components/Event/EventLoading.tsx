b8c6e2cc0b99b182e573074beed7de2103fe66a4
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-27 13:36:53 -0500

    Event loading component

---
+++ components/Event/EventLoading.tsx
@@ -0,0 +1,47 @@
+import { Box, Stack } from "@chakra-ui/react";
+
+export default function EventLoading() {
+  return (
+    <Box>
+      <Stack direction="row" spacing={2}>
+        <Box className="skeletonLoading" w="95px" h="18px" rounded="lg"></Box>
+        <Box className="skeletonLoading" w="95px" h="18px" rounded="lg"></Box>
+      </Stack>
+
+      <Box
+        className="skeletonLoading"
+        maxW="sm"
+        h="32px"
+        mt="7"
+        rounded="md"
+      ></Box>
+      <Box
+        className="skeletonLoading"
+        w="120px"
+        h="20px"
+        mt="2"
+        rounded="md"
+      ></Box>
+
+      <Box
+        className="skeletonLoading"
+        maxW="80%"
+        h="17px"
+        mt="3"
+        rounded="md"
+      ></Box>
+      <Box
+        className="skeletonLoading"
+        maxW="57%"
+        h="17px"
+        mt="1"
+        rounded="md"
+      ></Box>
+
+      <Stack direction="row" spacing={2} justifyContent="flex-end" mt="5">
+        <Box className="skeletonLoading" w="85px" h="28px" rounded="lg"></Box>
+        <Box className="skeletonLoading" w="85px" h="28px" rounded="lg"></Box>
+      </Stack>
+    </Box>
+  );
+}
