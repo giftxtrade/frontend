7352b42965f745771ae3b77b37868c37487f406b
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-09-28 08:23:41 -0500

    Remove SS rendering

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,20 +1,17 @@
 import { authStore, logout } from '../store/auth-store';
 import router from "next/router";
-import { useCookies } from "react-cookie";
-import { Flex, Spinner } from '@chakra-ui/react'
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { serialize } from 'cookie';
-import { DocumentContext } from 'next/document';
+import { Flex, Spinner } from "@chakra-ui/react";
+import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Logout() {
-  const [cookie, setCookie, removeCookie] = useCookies(['access_token'])
   useEffect(() => {
-    authStore.dispatch(logout())
-    localStorage.removeItem("inviteCode");
+    authStore.dispatch(logout());
+    localStorage.removeItem("invite_code");
     localStorage.removeItem("redirect");
+    localStorage.removeItem("access_token");
     router.push("/");
-  }, [])
+  }, []);
 
   return (
     <>
@@ -22,21 +19,9 @@ export default function Logout() {
         <title>Logging out...</title>
       </Head>
 
-      <Flex alignItems="center" justifyContent='center' p='20' >
-        <Spinner size='xl' />
+      <Flex alignItems="center" justifyContent="center" p="20">
+        <Spinner size="xl" />
       </Flex>
     </>
-  )
+  );
 }
-
-export const getServerSideProps = async (ctx: DocumentContext) => {
-  if (ctx.res) {
-    ctx.res.setHeader('Set-Cookie', [
-      serialize('access_token', '', {
-        maxAge: -1,
-        path: '/',
-      }),
-    ])
-  }
-  return { props: {} }
-};
