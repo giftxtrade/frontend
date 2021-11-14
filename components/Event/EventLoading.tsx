5b9a7c9994467bffafb815c34128d028880934a9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-14 17:56:25 -0600

    refactor: self closing Box component

--- components/Event/EventLoading.tsx
+++ components/Event/EventLoading.tsx
@@ -4,24 +4,12 @@ export default function EventLoading() {
   return (
     <Box>
       <Stack direction="row" spacing={2}>
-        <Box className="skeletonLoading" w="95px" h="18px" rounded="lg"></Box>
-        <Box className="skeletonLoading" w="95px" h="18px" rounded="lg"></Box>
+        <Box className="skeletonLoading" w="95px" h="18px" rounded="lg" />
+        <Box className="skeletonLoading" w="95px" h="18px" rounded="lg" />
       </Stack>
 
-      <Box
-        className="skeletonLoading"
-        maxW="sm"
-        h="32px"
-        mt="7"
-        rounded="md"
-      ></Box>
-      <Box
-        className="skeletonLoading"
-        w="120px"
-        h="20px"
-        mt="2"
-        rounded="md"
-      ></Box>
+      <Box className="skeletonLoading" maxW="sm" h="32px" mt="7" rounded="md" />
+      <Box className="skeletonLoading" w="120px" h="20px" mt="2" rounded="md" />
 
       <Box
         className="skeletonLoading"
@@ -29,19 +17,19 @@ export default function EventLoading() {
         h="17px"
         mt="3"
         rounded="md"
-      ></Box>
+      />
       <Box
         className="skeletonLoading"
         maxW="57%"
         h="17px"
         mt="1"
         rounded="md"
-      ></Box>
+      />
 
       <Stack direction="row" spacing={2} justifyContent="flex-end" mt="5">
-        <Box className="skeletonLoading" w="85px" h="28px" rounded="lg"></Box>
-        <Box className="skeletonLoading" w="85px" h="28px" rounded="lg"></Box>
+        <Box className="skeletonLoading" w="85px" h="28px" rounded="lg" />
+        <Box className="skeletonLoading" w="85px" h="28px" rounded="lg" />
       </Stack>
     </Box>
-  );
+  )
 }
